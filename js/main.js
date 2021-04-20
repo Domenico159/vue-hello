f0fa3594fc1a5008179d63702af4fb45d66a06ec
Domenico159 <domenico.zummo@gmail.com>
2021-04-20 16:05:45 +0200

    Project completed

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+const main = new Vue(
+
+    {
+
+        el:'#main',
+        data:{
+
+            tittle:'Viaggi dopo il covid',
+            img1:'https://content.r9cdn.net/rimg/himg/f3/bc/59/leonardo-1105322-1105322cdmssabrevfm_img_pho_000_nc__505642_BORExterior___S-856391.jpg?width=660&height=400&xhint=860&yhint=509&crop=true%20&outputtype=webp',
+            img2:'https://www.thaiairways.com/static/it_IT/imgscontent/Lifestyle/BALI_635x450.jpg',
+            img3:'https://images.lonelyplanetitalia.it/static/places/maldive-139.jpg?q=80&s=5ae7eea4a1747b04168bbe2b7af8e628',
+            classImg:'box-img',
+
+        },
+        // methods:{
+        //     transition(){
+        //          setInterval(() =>{
+
+        //             if(this.classImg1 === 'box-img img1'){
+        //                 return this.classImg1 = 'box-img img1 active';
+        //             }else {
+        //                 return this.classImg1 = 'box-img img1';
+        //             }
+
+        //         } ,2000)
+        //     },
+        // },
+
+    },
+
+);
